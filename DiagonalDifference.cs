795261ce6c54a702ceec49d2a172a9c42e046287
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-06-23 08:24:14 -0300

    Desafio diferença de diagonais

---
+++ DiagonalDifference.cs
@@ -0,0 +1,30 @@
+﻿public static int diagonalDifference(List<List<int>> arr)
+{
+    var linhas = arr.Count - 1;
+        
+    var colunas = arr[0].Count - 1;
+
+    int aux = colunas;
+
+    var diagonalPrincipal = 0;
+
+    var diagonalSecundaria = 0;
+
+    for(var i = 0; i <= linhas; i++)
+    {
+        for(var j = 0; j <= colunas; j++)
+        {
+            if(i == j)
+            {
+                diagonalPrincipal += arr[i][j];
+                diagonalSecundaria += arr[i][aux];
+                aux--;
+                break;
+            }
+        }
+    }
+
+    var diferenca = diagonalPrincipal - diagonalSecundaria;
+    
+    return diferenca < 0 ? diferenca * -1 : diferenca;
+}
